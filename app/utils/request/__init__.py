b13086b0877bd0683c6d6797fa46813f9b96195e
chen21439 <14557167+chen21439@user.noreply.gitee.com>
2025-11-22 10:32:04 +0800

    功能添加

--- app/utils/request/__init__.py
+++ app/utils/request/__init__.py
@@ -4,9 +4,23 @@
 """
 
 from .ai_client import AIClient, get_global_client, reset_global_client
+from .batch_manager import BatchManager, get_default_batch_manager, reset_default_batch_manager
+from .batch_processor import (
+    BatchProcessor,
+    get_default_batch_processor,
+    reset_default_batch_processor,
+    merge_candidates_with_dedup
+)
 
 __all__ = [
     "AIClient",
     "get_global_client",
-    "reset_global_client"
+    "reset_global_client",
+    "BatchManager",
+    "get_default_batch_manager",
+    "reset_default_batch_manager",
+    "BatchProcessor",
+    "get_default_batch_processor",
+    "reset_default_batch_processor",
+    "merge_candidates_with_dedup"
 ]
--- app/utils/unTaggedPDF/__init__.py
+++ app/utils/unTaggedPDF/__init__.py
@@ -1,5 +1,5 @@
 """
-unTaggedPDF - PDF表格提取工具模块
+PDF表格提取工具模块
 """
 
 from .table_extractor import TableExtractor
